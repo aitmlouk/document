70fa174abf00538513bc4a4a4f646f43fc7a64c4
Ait-Mlouk Addi <aitmlouk@gmail.com>
2018-11-25 12:54:06 +0100

    [ADD] app cmis_alf

---
+++ cmis_alf/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2016 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'CMIS for Alfresco',
+    'version': '10.0.3.0.0',
+    'summary': 'Alfresco extension for the CMIS Connector',
+    'category': 'Document Management',
+    'author': "ACSONE SA/NV",
+    'website': 'http://alfodoo.org/',
+    'license': 'AGPL-3',
+    'depends': [
+        'cmis',
+    ],
+    'data': [
+        'views/cmis_backend_view.xml',
+    ],
+    'images': [
+        'static/description/main_icon.png',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
